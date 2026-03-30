449f7cf51b7f6df298435ab13e03fd73cc0a2292
zod01 <rajhesh.panchanadhan@gmail.com>
2026-03-30 09:26:40 +0530

    chore: bump ziba to v1.2.0

--- Casks/ziba.rb
+++ Casks/ziba.rb
@@ -1,8 +1,8 @@
 cask "ziba" do
-  version "1.1.0"
-  sha256 "bc0fe1fea3c958839cf8353c9059f2210bea9429dd8caa657e526e1e5e5eaca2"
+  version "1.2.0"
+  sha256 "897d9f2cd4bf31a7600ccd6980c659cc11499bafb41926cb912f57f73df38d66"
 
-  url "https://github.com/electricsheepco/ziba/releases/download/v#{version}/ziba.dmg"
+  url "https://github.com/electricsheepco/ziba/releases/download/v#{version}/ziba-#{version}.dmg"
   name "ziba"
   desc "Art wallpaper app powered by WikiArt"
   homepage "https://github.com/electricsheepco/ziba"
