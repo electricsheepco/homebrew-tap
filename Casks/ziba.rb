177829430790d9c5494f15ea8b3c250bd08449a1
zod01 <rajhesh.panchanadhan@gmail.com>
2026-04-21 21:44:29 +0530

    chore: bump ziba to 1.2.2

--- Casks/ziba.rb
+++ Casks/ziba.rb
@@ -1,6 +1,6 @@
 cask "ziba" do
-  version "1.2.1"
-  sha256 "d778a5f5516b6fa35408fe2304378af9bed4f8f617874322ee5c050c39da58e7"
+  version "1.2.2"
+  sha256 "ea33db1f55d409418f2235d0f54c7d32f403fbdccd07ba739fbeb74f989380ee"
 
   url "https://github.com/electricsheepco/ziba/releases/download/v#{version}/ziba-#{version}.dmg"
   name "ziba"
